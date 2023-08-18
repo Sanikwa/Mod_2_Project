db7cb3f939d8f4df74b5621f43bbd8188f9d8546
Sanikwa <bornqueen22@gmail.com>
2023-08-18 11:50:59 -0400

    first commit

---
+++ src/products.js
@@ -0,0 +1,23 @@
+import img1 from './assets/img1.jfif';
+import img2 from './assets/img2.jfif';
+import img3 from './assets/img3.jfif';
+export const PRODUCTS =[
+  {
+    id: 1,
+    productName: "Plate Set",
+    price: 12,
+    productImage: img1
+  },
+{
+    id: 2,
+    productName: "Dining Set for 4",
+    price: 99,
+    productImage: img2
+  },
+{
+    id: 3,
+    productName: "Table Cloth",
+    price: 8,
+    productImage: img3
+  },
+]
